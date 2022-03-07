f41a373c719b3eef1e83b6e664a121c90dc05684
pariyaard <1024342@mborijnland.nl>
2022-03-07 12:28:08 +0100

    made table, controller etc

--- PariyaDerakhshan/routes/web.php
+++ PariyaDerakhshan/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use http\Client\Curl\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource(‘users’, Controller::class);
